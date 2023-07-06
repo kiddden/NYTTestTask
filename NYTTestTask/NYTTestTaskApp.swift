5af1d11cab9507acbcba9147b29ae017bef34cd5
kiddden <kazzer@knu.ua>
2023-07-06 10:54:13 +0300

    Initial Commit

---
+++ NYTTestTask/NYTTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTTestTaskApp.swift
+//  NYTTestTask
+//
+//  Created by Eugene Ned on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NYTTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
